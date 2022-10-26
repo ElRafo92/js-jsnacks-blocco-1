e26c298c1aef93b9d609910450c78c35a8419895
ElRafo92 <rafael.martinez@outlook.it>
2022-10-26 11:40:20 +0200

    ex 2 e 3

---
+++ jsnack2/script2.js
@@ -0,0 +1,17 @@
+let primaParola = prompt("");
+let length1 = primaParola.length
+console.log(length1);
+
+let secondaParola = prompt("");
+let length2 = secondaParola.length;
+console.log(length2);
+
+if (length1 > length2) {
+    console.log("prima parola più lunga")
+} else if (length1 > length2) {
+    console.log("seconda parola più lunga")
+} else {
+    console.log("lunghe uguali")
+}
+
+
---
+++ jsnack3 copy/script3.js
@@ -0,0 +1,7 @@
+for (i=0; i<11; i++) {
+    let parola = prompt("scrivi un numero");
+    
+    
+}
+parola = i+i
+console.log(parola)
